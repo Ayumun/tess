5715d35cf27ebb12623dd59c96198d9b8fcbe0a2
Ayumun <47070528+Ayumun@users.noreply.github.com>
2022-11-20 19:40:09 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>CodeSanbboxとGithubを連携してみた</h1>
+      <p>試しに変更を加えました!!</p>
+    </div>
+  );
+}
